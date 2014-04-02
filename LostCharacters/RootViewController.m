32469438f4cb5cef1597e6b1028053e402037cd5
Charles Northup <cjnorth40@gmail.com>
2014-04-01 21:36:20 -0500

    need to fix filter bug

--- LostCharacters/RootViewController.m
+++ LostCharacters/RootViewController.m
@@ -16,7 +16,7 @@
 }
 @property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
 @property (weak, nonatomic) IBOutlet UIToolbar *bottomToolBar;
-@property NSArray* characters;
+@property NSMutableArray* characters;
 @property (weak, nonatomic) IBOutlet UITableView *myTableView;
 @property (weak, nonatomic) IBOutlet UITextField *passengerTextField;
 @property (weak, nonatomic) IBOutlet UITextField *actorTextField;
@@ -33,6 +33,7 @@
 - (void)viewDidLoad{
     
     [super viewDidLoad];
+    aliveOrDead = [NSNumber numberWithInt:1];
     editing = NO;
     [self load];
     self.toolBar.hidden =! editing;
@@ -47,12 +48,13 @@
     NSSortDescriptor* sortDescriptor1 = [[NSSortDescriptor alloc] initWithKey:@"actor" ascending:YES];
     NSSortDescriptor* sortDescriptor2 = [[NSSortDescriptor alloc] initWithKey:@"passenger" ascending:YES];
     NSArray* sortDescriptors = [NSArray arrayWithObjects:sortDescriptor1, sortDescriptor2, nil];
-    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"passenger contains[cd] \"%@\"", self.searchQuery];
-    [request setPredicate:predicate];
+//    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(passenger CONTAINS[c] %@) OR (actor CONTAINS[c] %@)", self.searchQuery, self.searchQuery];
+//    [request setPredicate:predicate];
+//    NSLog(@"%@", request.predicate);
     request.sortDescriptors = sortDescriptors;
     NSArray* charactersInDataCore = [self.managedObjectContext executeFetchRequest:request error:nil];
     if (charactersInDataCore.count) {
-        self.characters = charactersInDataCore;
+        self.characters = (NSMutableArray*)charactersInDataCore;
     }
     else{
         LostCharacter* initialCharacter1 = [NSEntityDescription insertNewObjectForEntityForName:@"LostCharacter" inManagedObjectContext:self.managedObjectContext];
@@ -68,7 +70,7 @@
         initialCharacter3.passenger = @"Hugo “Hurley” Reyes";
         initialCharacter3.dead = 0;
         [self.managedObjectContext save:nil];
-        self.characters = @[initialCharacter2, initialCharacter3, initialCharacter1];
+        self.characters = [NSMutableArray arrayWithObjects:initialCharacter2, initialCharacter3, initialCharacter1, nil];
     }
     [self.myTableView reloadData];
 }
@@ -77,6 +79,7 @@
     LostCharacter* character = [NSEntityDescription insertNewObjectForEntityForName:@"LostCharacter" inManagedObjectContext:self.managedObjectContext];
     character.actor = [self.actorTextField.text capitalizedString];
     character.passenger = [self.passengerTextField.text capitalizedString];
+    character.dead = aliveOrDead;
     [self.managedObjectContext save:nil];
     self.actorTextField.text = nil;
     self.passengerTextField.text = nil;
@@ -102,6 +105,16 @@
     self.toolBar.hidden =! editing;
     self.bottomToolBar.hidden =!editing;
     self.addButton.enabled = editing;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.managedObjectContext deleteObject:self.characters[indexPath.row]];
+        [self.managedObjectContext save:nil];
+        [self load];
+        [self.myTableView reloadData];
+    }
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -146,9 +159,57 @@
 
 }
 
+-(void)filter:(NSString *)searchedTextData //filter method helper
+{
+    NSMutableArray *filteredTableData = [[NSMutableArray alloc] init];
+    
+    // Create our fetch request
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    // Define the entity we are looking for
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"LostCharacter" inManagedObjectContext:self.managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+    
+    // Define how we want our entities to be sorted
+    NSSortDescriptor* sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"passenger" ascending:YES];
+    NSArray* sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor, nil];
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    // If we are searching for anything...
+    if(searchedTextData.length > 0)
+    {
+        // Define how we want our entities to be filtered
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(passenger CONTAINS[c] %@) OR (actor CONTAINS[c] %@)", searchedTextData, searchedTextData];
+        [fetchRequest setPredicate:predicate];
+    }
+    
+    NSError *error;
+    
+    // Finally, perform the load
+    NSArray *loadedEntities = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
+    filteredTableData = [[NSMutableArray alloc] initWithArray:loadedEntities];
+    
+    self.characters = filteredTableData;
+    
+    NSLog(@"Filtered Data -->, %@", filteredTableData);
+    NSLog(@"Text Search->, %@", searchedTextData);
+    
+    //[self load];
+    [self.myTableView reloadData];
+    
+    
+    
+}
+
 -(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
-    self.searchQuery = searchText;
+    
+    if (![searchText isEqualToString:@""]) {
+        [self filter:searchText];
+    }
+    else{
     [self load];
+    }
 }
 
 @end
